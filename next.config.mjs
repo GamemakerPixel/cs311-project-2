676bdb0b5efd66c0951a31935a8d0d83d1d7ac1d
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-10-17 01:28:02 -0400

    Setup Github to automatically deploy the webapp to Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// Learned how to export static files from https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/
+const nextConfig = {
+  basePath: "/cs311-project-2"
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
