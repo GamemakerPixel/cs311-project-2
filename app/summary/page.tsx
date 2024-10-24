65661b73ef949137ac7377eefd941dc4f5d5cfae
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-10-24 02:21:08 -0400

    Enable automatic color generation for styling on summary page

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,25 +1,69 @@
 'use client'
 import { useState, useEffect } from 'react';
 
+import { Card, getCards } from '@/app/components/cards'
 
-interface Card {
-  question: string
-  answer: string
-  understanding: number
+
+function generatePercentageColor(time: number) {
+  // RGB Values
+  const gradient = [
+    {
+      time: 0.0,
+      color: [235, 93, 89], //"hard" color
+    },
+    {
+      time: 0.5,
+      color: [235, 198, 89], //"medium" color
+    },
+    {
+      time: 1.0,
+      color: [153, 235, 89], //"easy" color
+    },
+  ]
+
+  function rgbToString(color: number[]) {
+    return "rgb(" + color.join(",") + ")"
+  }
+
+  for (let keyIndex = 1; keyIndex < gradient.length; keyIndex++) {
+    if (time < gradient[keyIndex].time) {
+      const startKey = gradient[keyIndex - 1]
+      const endKey = gradient[keyIndex]
+      const relativeTime = (time - startKey.time) / (endKey.time - startKey.time)
+
+      let newColor: number[] = []
+      for (let channelIndex = 0; channelIndex < startKey.color.length; channelIndex++) {
+        newColor.push(Math.round(
+          startKey.color[channelIndex] * (1.0 - relativeTime)
+          + endKey.color[channelIndex] * relativeTime
+        ))
+      }
+
+      return rgbToString(newColor)
+    }
+    else if (time == gradient[keyIndex].time) {
+      return rgbToString(gradient[keyIndex].color)
+    }
+  }
+
+  return "rgb(255,0,255)"
+
+
 }
 
 
-interface CardReportProps {
-  card: Card
-}
-
-
-function CardReport({card}: CardReportProps) {
+function CardReport({ card }: {card: Card}) {
   const percentUnderstood = Math.round(card.understanding * 100)
+  //const bgClass = "bg-[" + generatePercentageColor(card.understanding) + "]"
+
+  //console.log(bgClass)
 
   return (
-    <div className="flex">
-      <div>
+    <div className="flex m-2 items-center">
+      <div
+        style={{ backgroundColor: generatePercentageColor(card.understanding)}}
+        className="p-1 rounded-lg mr-2 w-20 text-center text-itext font-bold"
+      >
         {percentUnderstood}%
       </div>
       <div>
@@ -31,19 +75,9 @@ function CardReport({card}: CardReportProps) {
 
 
 export default function Summary() {
-  const [cards, setCards] = useState<Card[]|null>(null)
+  const cards = getCards()
 
-  useEffect(() => {
-    async function fetchCards() {
-      let cardsResponse = await fetch('/cards.json')
-      let cards: Card[] = await cardsResponse.json()
-      
-      setCards(cards);
-    }
-    fetchCards()
-  }, [])
-
-  if (cards == undefined) {
+  if (cards == null) {
     return <div>Loading cards...</div>
   }
 
@@ -55,7 +89,7 @@ export default function Summary() {
   return (
     <div>
       <h1>Summary</h1>
-      <div>
+      <div className="bg-secondary m-8 p-4 rounded-lg">
         {
           cards.map((card: Card) => (
             <CardReport card={card}/>
