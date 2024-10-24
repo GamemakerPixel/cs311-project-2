47754bebab77bbd3754a89e188bf7f1ecaf56755
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-10-24 02:20:41 -0400

    Improve styling of navigation bar

--- app/components/navigation_bar.tsx
+++ app/components/navigation_bar.tsx
@@ -6,30 +6,26 @@ export default function NavigationBar() {
   const titlePath = "/"
 
   interface Page {
-    displayName: string
+    name: string
     path: string
   }
 
-  const pages = [
+  const pages: Page[] = [
     {name: "Add Card", path: "/add"},
     {name: "Study", path: "/study"},
     {name: "Summary", path: "/summary"},
   ]
 
-
-  function bleh() {
-    return <div>turt</div>
-  }
-
-
   return (
-    <nav className="bg-secondary h-20">
-      <Link href={titlePath}>
+    <nav className="bg-secondary flex content-center justify-center">
+      <Link href={titlePath}
+        className="text-xl text-accent text-center m-2 inline-block py-1 drop-shadow-[0_0_5px_#e7513388]"
+      >
         {title}
       </Link>
       {
         pages.map((page) => (
-          <Link href={page.path} className="mx-4">
+          <Link href={page.path} className="m-2 text-center bg-background p-2 rounded-lg">
             {page.name}
           </Link>
         ))
